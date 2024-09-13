ab76c4e7399e3389ecb4cdf1a982edeef3b3935b
Harshit Singh <harshi6196@gmail.com>
2024-09-13 02:27:21 -0400

    feat: added location and rating text. refactor: changed className for EventCard

--- src/components/EventCard/EventCard.jsx
+++ src/components/EventCard/EventCard.jsx
@@ -1,16 +1,24 @@
 import { card } from "../../utils/constants/constants";
 import "./EventCard.css";
+import locationPic from "../../assets/location-pointer.svg"
 
 function EventCard(card) {
   return (
     <>
       <div className="card">
-        <img className="card-img" src={card.image} alt={card.alt}></img>
-        <div className="card-content">
-          <h2 className="card-title">{card.title}</h2>
-          <p className="card-description">{card.description}</p>
+        <img className="card__img" src={card.image} alt={card.alt}></img>
+        <div className="card__content">
+          <h2 className="card__title">{card.title}</h2>
+          <p className="card__description">{card.description}</p>
+        
+          <div className="card__footer">
+            <div className="card__location">
+              <img src={locationPic} alt="" className="card__location-img" />
+              <p className="card__location-name">{card.location}</p>
+            </div>
+            <p className="card__rating">{card.rating}</p>
+          </div>
         </div>
-
       </div>
     </>
   );
