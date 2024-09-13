3590783ca44f5e98d19bc421883898bfacfc644e
Harshit Singh <harshi6196@gmail.com>
2024-09-12 23:52:58 -0400

    feat: added Card Component with Location, Title, Image.

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -6,8 +6,7 @@ function Main() {
     <>
       <main className="main">
         Main
-        <EventCard />
-      </main>
+        <EventCard image="https://www.canr.msu.edu/contentAsset/image/a934ada7-5812-4669-ab1a-573e28d8547e/fileAsset/filter/Resize,Jpeg/resize_w/750/jpeg_q/80" alt="business-image" location="Miami, Florida" description="Buesiness Event THings Buesiness Event THings Buesiness Event THings Buesiness Event THings Buesiness Event THings Buesiness Event THings Buesiness Event THings Buesiness Event THings Buesiness Event THings Buesiness Event THings Buesiness Event THings Buesiness Event THings Buesiness Event THings Buesiness Event THings Buesiness Event THings Buesiness Event THings Buesiness Event THings Buesiness Event THings Buesiness Event THings Buesiness Event THings Buesiness Event THings Buesiness Event THings Buesiness Event THings Buesiness Event THings Buesiness Event THings Buesiness Event THings Buesiness Event THings Buesiness Event THings "/>      </main>
     </>
   );
 }
