66f4a45d30d049a56f3344ef4bd4b6099919047e
6ent1 <gentitaga@outlook.com>
2024-09-14 05:33:16 -0400

    Add header video and other minor fixes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,14 @@
+import heroVideo from "../../assets/HeroVideo.mp4";
 import "./Header.css";
 
 function Header() {
   return (
     <>
       <header className="header">
+        <video autoPlay loop muted playsInline className="header__video">
+          <source src={heroVideo} type="video/mp4"></source>
+        </video>
+
         <h1 className="header__title">
           Everything Fall - The best place to post and find the perfect fall
           festival
