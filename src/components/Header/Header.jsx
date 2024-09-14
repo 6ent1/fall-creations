8b4008edee51a7e119a1f0e2e7e055724b55d804
Josiah Pedersen <josiahpedersen8@gmail.com>
2024-09-13 20:58:18 -0600

    start of header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,21 @@ import "./Header.css";
 function Header() {
   return (
     <>
-      <header className="header">Header</header>
+      <header className="header">
+        <h1 className="header__title">
+          Everything Fall - The best place to post and find the perfect fall
+          festival
+        </h1>
+        <label htmlFor="search-input" className="header__input">
+          <input
+            type="search"
+            name="search"
+            id="search-input"
+            className="header__input"
+            placeholder="Search for an event"
+          />
+        </label>
+      </header>
     </>
   );
 }
