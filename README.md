04240c0a01dc2b0bb9a1880971346f991a441c95
6ent1 <gentitaga@outlook.com>
2024-09-15 15:15:36 -0400

    Add ReadMe info and update header carousel

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
 # Fall Voyage
 
-A website where businesses post events related to the fall season and everyone can find something they like to do around their location.
+Welcome to Fall Voyage, a web application that allows users to discover and explore exciting fall events from various businesses. Whether you're looking to attend a local festival, pumpkin patch, or any seasonal activity, you'll find it here!
+
+## Features
+
+- **Event Listings:** Browse a curated list of fall events from different businesses.
+- **Location Filtering:** Easily filter events based on your location to find the best activities near you.
+- **Light/Dark Mode:** Switch between light and dark themes for a comfortable browsing experience.
+- **Responsive Design:** The application is designed to work seamlessly on both desktop and mobile devices.
+
+## Tech Stack
+
+- **React:** A JavaScript library for building user interfaces.
+- **Vite:** A fast build tool and development server for modern web projects.
+- **JavaScript:** The programming language used for application logic.
+- **CSS:** For styling the application.
+- **BEM Methodology:** Followed BEM (Block Element Modifier) guidelines for CSS class naming to maintain organized and scalable styles.
 
 ## How to run the project
 
@@ -10,7 +25,7 @@ To install dependencies on your computer run the following command:
   npm install
 ```
 
-To run the project on your browser on your computer run the following command:
+To run the project on your browser run the following command:
 
 ```bash
   npm run dev
--- src/utils/constants/constants.js
+++ src/utils/constants/constants.js
@@ -1,67 +1,102 @@
-const card = {
-  title: "Pumpkin Patch Festival",
-  description: "A fun day at the pumpkin patch with family activities.",
-  location: "California",
-  type: "Festival",
-  initialRating: 4,
-  image:
-    "https://cdn.pixabay.com/photo/2021/09/20/16/39/pumpkins-6641293_1280.jpg",
-};
-
 export const events = [
   {
-    title: "Pumpkin Patch Festival",
+    title: "Corn Maze",
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    location: "California",
+      "The Corn Maze is a fun festival for the whole family! Find your way through the Corn Maze and enjoy the other activities we have to offer.",
+    location: "Florida",
     type: "Festival",
     initialRating: 4,
     picture:
-      "https://cdn.pixabay.com/photo/2021/09/20/16/39/pumpkins-6641293_1280.jpg",
+      "https://cdn.pixabay.com/photo/2015/08/17/15/41/corn-892595_1280.jpg",
   },
   {
-    title: "Harvest Fair",
-    description: "Enjoy local food and music at the Harvest Fair.",
-    location: "New York",
-    type: "Fair",
+    title: "Fall Film Festival",
+    description:
+      "Enjoy watching Fall Film Classics with your Friends and Family!",
+    location: "Georgia",
+    type: "Festival",
     initialRating: 5,
     picture:
-      "https://cdn.pixabay.com/photo/2023/10/30/18/59/pumpkins-8353576_1280.jpg",
+      "https://cdn.pixabay.com/photo/2013/02/26/01/10/auditorium-86197_1280.jpg",
   },
   {
-    title: "Autumn Lights Show",
-    description: "A stunning light display showcasing the beauty of fall.",
-    location: "Colorado",
+    title: "Haunted Harvest Fest",
+    description:
+      "Join us for a spooky festival filled with eerie fun for all ages. Enjoy our haunted activities and costume contests!",
+    location: "Texas",
+    type: "Festival",
+    initialRating: 4,
+    picture:
+      "https://cdn.pixabay.com/photo/2019/11/03/05/53/pumpkin-4597886_1280.jpg",
+  },
+  {
+    title: "Slice of Autumn",
+    description:
+      "Participate in this tasty competition on who will create the best slice of Pumpkin Pie!",
+    location: "Texas",
+    type: "Tasting",
+    initialRating: 3,
+    picture:
+      "https://cdn.pixabay.com/photo/2012/12/24/08/39/pie-72274_1280.jpg",
+  },
+  {
+    title: "Autumn Artisan Market",
+    description:
+      "View the unique and handcrafted works made by the people in our community, browse sections of local craft, decor, and treats!",
+    location: "Alabama",
+    type: "Fair",
+    initialRating: 4,
+    picture:
+      "https://cdn.pixabay.com/photo/2016/11/19/10/01/art-1838414_1280.jpg",
+  },
+  {
+    title: "Ember Glow Gathering",
+    description:
+      "Spend time with your family and friends near the bonfire making smores and watching a live performance by our local talent!",
+    location: "Texas",
+    type: "Show",
+    initialRating: 5,
+    picture:
+      "https://cdn.pixabay.com/photo/2016/11/29/04/17/bonfire-1867275_1280.jpg",
+  },
+  {
+    title: "Apple Orchard Picking",
+    description:
+      "Enjoy a fun-filled day of apple picking and fall festivities with your family and friends. Savor the taste of fresh, crisp apples picked you!",
+    location: "Arkansas",
+    type: "Fair",
+    initialRating: 3,
+    picture:
+      "https://cdn.pixabay.com/photo/2015/10/24/20/03/apples-1004886_1280.jpg",
+  },
+  {
+    title: "Scarecrow Contest",
+    description:
+      "Join us in this contest where creativity is the main attribute. View your local artists as they try to create the best designed scarecrow!",
+    location: "Alabama",
     type: "Show",
     initialRating: 3,
     picture:
-      "https://cdn.pixabay.com/photo/2021/09/20/16/39/pumpkins-6641293_1280.jpg",
+      "https://cdn.pixabay.com/photo/2019/09/17/19/53/garden-4484413_1280.jpg",
   },
   {
-    title: "Autumn Lights Show",
-    description: "A stunning light display showcasing the beauty of fall.",
-    location: "Colorado",
+    title: "Pumpkin Carving Contest",
+    description:
+      "Help us decide who in your community created the most creative Jack O' Lantern!",
+    location: "Texas",
     type: "Show",
-    initialRating: 3,
-    picture:
-      "https://cdn.pixabay.com/photo/2021/09/20/16/39/pumpkins-6641293_1280.jpg",
-  },
-  {
-    title: "Wine Tasting",
-    description: "Sample the best wines from local vineyards.",
-    location: "California",
-    type: "Tasting",
     initialRating: 4,
     picture:
-      "https://cdn.pixabay.com/photo/2021/09/20/16/39/pumpkins-6641293_1280.jpg",
+      "https://cdn.pixabay.com/photo/2015/10/24/21/22/pumpkin-1004973_1280.jpg",
   },
   {
-    title: "Wine Tasting",
-    description: "Sample the best wines from local vineyards.",
-    location: "New Jersey",
-    type: "Tasting",
-    initialRating: 4,
+    title: "Tales Across the Bonfire",
+    description:
+      "Enjoy a night sitting by the bonfire telling your spookiest stories around others.",
+    location: "Oregon",
+    type: "Show",
+    initialRating: 5,
     picture:
-      "https://cdn.pixabay.com/photo/2013/05/15/06/10/autumn-111315_1280.jpg",
+      "https://cdn.pixabay.com/photo/2022/11/03/13/07/book-7567437_1280.jpg",
   },
 ];
